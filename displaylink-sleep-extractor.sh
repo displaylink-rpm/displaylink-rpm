383b99a0fd55be7b51252c99266c5933801bb042
chuma <victor@chuma.org>
2016-10-04 10:48:26 -0400

    Updated sleep script extractor to work with DisplayLink driver 1.2.58 - script name changed to suspend.sh

--- displaylink-sleep-extractor.sh
+++ displaylink-sleep-extractor.sh
@@ -2,7 +2,7 @@
 file=$1
 
 startline=$(grep -n "add_pm_script()" "$file" | cut -d: -f1 |head -1)
-endline=$(grep -n 'chmod 0755 $COREDIR/displaylink.sh' "$file" | cut -d: -f1 |head -1)
+endline=$(grep -n 'chmod 0755 $COREDIR/suspend.sh' "$file" | cut -d: -f1 |head -1)
 
 source <(
 	tail -n +$startline $file | head -n +$(($endline - $startline)) &&
@@ -11,6 +11,6 @@ source <(
 COREDIR=$(mktemp -d)
 add_pm_script "systemd"
 
-cat "$COREDIR/displaylink.sh"
+cat "$COREDIR/suspend.sh"
 
 rm -rf "$COREDIR"
