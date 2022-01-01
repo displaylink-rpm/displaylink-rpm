048bc85ea2e33b23c9b0d32160b60a3fcbf954b8
Michael L. Young <elgueromexicano@gmail.com>
2021-12-31 23:26:09 -0500

    Cleanup of CI scripts while looking through logs (#192)
    * Remove line in ci scripts to change to a non-existence directory.
    
    * Some cleanups to CI scripts
    
    * Some more cleanups of errors being found
    
    * Fix specifying a make target

--- ci/centos-stream.sh
+++ ci/centos-stream.sh
@@ -2,10 +2,8 @@
 
 dnf install -y gcc gcc-c++ libdrm-devel rpm-build make wget dnf-utils --enablerepo=extras
 
-cd /base-src
-
-dnf-builddep -y ./displaylink.spec
+dnf builddep -y ./displaylink.spec
 
 chown `id -u`:`id -g` -R .
 
-make $(SPECIFICTARGET)
+make $SPECIFICTARGET
--- ci/centos.sh
+++ ci/centos.sh
@@ -2,10 +2,8 @@
 
 yum install -y gcc gcc-c++ libdrm-devel rpm-build make wget yum-utils --enablerepo=extras
 
-cd /base-src
-
 yum-builddep -y ./displaylink.spec
 
 chown `id -u`:`id -g` -R .
 
-make $(SPECIFICTARGET)
+make $SPECIFICTARGET
