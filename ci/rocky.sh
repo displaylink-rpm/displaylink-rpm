9d4e8970c0c70682ff1bc98bb090c41e0ba09905
Michael L. Young <elgueromexicano@gmail.com>
2021-12-31 22:17:47 -0500

    Some more cleanups of errors being found

--- ci/rocky.sh
+++ ci/rocky.sh
@@ -2,7 +2,7 @@
 
 dnf install -y gcc gcc-c++ libdrm-devel rpm-build make wget dnf-utils --enablerepo=extras
 
-dnf-builddep -y ./displaylink.spec
+dnf builddep -y ./displaylink.spec
 
 chown `id -u`:`id -g` -R .
 
