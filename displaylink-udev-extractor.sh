623c149a0816de5050cbd4050c83fbc81475766b
Michael L. Young <elgueromexicano@gmail.com>
2022-03-01 22:17:29 -0500

    Fixup the extract scripts.

--- displaylink-udev-extractor.sh
+++ displaylink-udev-extractor.sh
@@ -3,24 +3,22 @@ file=$1
 
 # Get systemd_start_stop_functions()
 startline=$(grep -n "systemd_start_stop_functions()" "$file" | cut -d: -f1 | head -1)
-endline=$(grep -m2 -n "^EOF$" "$file" | tail -n1 | cut -d: -f1 | head -1)
+endline=$(grep -n "displaylink_bootstrapper_code()" "$file" | cut -d: -f1 | head -1)
 
 source <(
-	tail -n +$startline $file | head -n +$(($endline - $startline)) &&
-	echo "}" # close the function op
+	tail -n +$startline $file | head -n +$(($endline - $startline - 1))
 )
 
 # Get displaylink_bootstrapper_code()
 startline=$(grep -n "displaylink_bootstrapper_code()" "$file" | cut -d: -f1 | head -1)
-endline=$(grep -n "chmod 0744 \"\$filename\"" "$file" | tail -n1 | cut -d: -f1 | head -1)
+endline=$(grep -n "chmod 0744 \"\$filename\"" "$file" | cut -d: -f1 | head -1)
 
 source <(
-	tail -n +$startline $file | head -n +$(($endline - $startline)) &&
-	echo "}" # close the function op
+	tail -n +$startline $file | head -n +$(($endline - $startline + 2))
 )
 
 COREDIR=$(mktemp -d)
-create_bootstrapper_code "systemd" "/usr/libexec/displaylink/udev.sh"
+create_bootstrap_file "systemd" "/usr/libexec/displaylink/udev.sh"
 
 sed -i -e '1 s/^.*$/\#!\/usr\/bin\/bash/' "$COREDIR/udev.sh"
 
