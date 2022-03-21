f782ed9315c999850382aca87a822a29efefa0a6
Michael L. Young <elgueromexicano@gmail.com>
2022-03-21 09:07:25 -0400

    Add builds for CentOS stream 9

--- ci/centos-stream.sh
+++ ci/centos-stream.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-dnf install -y gcc gcc-c++ libdrm-devel rpm-build make wget dnf-utils git --enablerepo=extras
+if [ "$OSVERSION" = "stream9" ]; then
+    dnf install -y rpm-build make gcc wget git 'dnf-command(builddep)'
+else
+    dnf install -y gcc gcc-c++ libdrm-devel rpm-build make wget dnf-utils git --enablerepo=extras
+fi
 
 dnf builddep -y ./displaylink.spec
 
