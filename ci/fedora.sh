dabc88932532d66b80f7028709b1c7db9b811e3f
Michael L. Young <elgueromexicano@gmail.com>
2023-12-25 10:36:48 -0500

    Remove distros that are EOL
    This will simplify the number of packages being built and supported.
    
    Resolves #210
    
    Signed-off-by: Michael L. Young <elgueromexicano@gmail.com>

--- ci/fedora.sh
+++ ci/fedora.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-dnf install -y rpm-build make gcc glibc-devel.x86_64 glibc-devel.i686 wget git 'dnf-command(builddep)'
+dnf install -y rpm-build make gcc gcc-c++ libdrm-devel systemd-rpm-macros glibc-devel.x86_64 glibc-devel.i686 wget git 'dnf-command(builddep)'
 
 dnf builddep -y --spec ./displaylink.spec
 
--- ci/fedora37plus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-dnf install -y rpm-build make gcc gcc-c++ libdrm-devel systemd-rpm-macros glibc-devel.x86_64 glibc-devel.i686 wget git 'dnf-command(builddep)'
-
-dnf builddep -y --spec ./displaylink.spec
-
-make $SPECIFICTARGET
