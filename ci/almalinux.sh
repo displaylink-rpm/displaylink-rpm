b34ad030e218f3c7be38ba54d8784d04069b6ded
Michael L. Young <elgueromexicano@gmail.com>
2024-03-09 10:19:00 -0500

    AlmaLinux needed to change their GPG key for version 8
    This is a workaround for that issue.
    
    For details:
    https://almalinux.org/blog/2023-12-20-almalinux-8-key-update/
    
    Signed-off-by: Michael L. Young <elgueromexicano@gmail.com>

--- ci/almalinux.sh
+++ ci/almalinux.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+## Workaround AlmaLinux 8 GPG key issue
+## https://almalinux.org/blog/2023-12-20-almalinux-8-key-update/
+rpm --import https://repo.almalinux.org/almalinux/RPM-GPG-KEY-AlmaLinux
+
 dnf install -y gcc gcc-c++ glibc-devel.x86_64 glibc-devel.i686 libdrm-devel rpm-build make wget dnf-utils git 'dnf-command(builddep)' --enablerepo=extras
 
 dnf builddep -y ./displaylink.spec
