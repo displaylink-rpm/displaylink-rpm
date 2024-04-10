c9a5439c452318311384dcc961622e8c1225e1da
Michael L. Young <elgueromexicano@gmail.com>
2024-04-10 12:58:53 -0400

    Adjust CI scripts to handle using Github for releases of evdi
    Signed-off-by: Michael L. Young <elgueromexicano@gmail.com>

--- ci/testbuilding.sh
+++ ci/testbuilding.sh
@@ -8,6 +8,7 @@ dnf builddep -y ./displaylink.spec
 
 chown `id -u`:`id -g` -R .
 
+if [ -z ${SPECIFICTARGET+x} ]; then
 echo "Testing 'make srpm'"
 make srpm
 
@@ -18,6 +19,12 @@ make rpm
 
 make clean-all
 
+echo "Testing 'make all'"
+make all
+
+make clean-all
+fi
+
 echo "Testing 'make srpm-github'"
 make srpm-github
 
@@ -25,11 +32,6 @@ make clean-all
 
 echo "Testing 'make rpm-github'"
 make rpm-github
-
-make clean-all
-
-echo "Testing 'make all'"
-make all
 
 make clean-all
 
