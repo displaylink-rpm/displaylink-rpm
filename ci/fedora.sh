bc843d8324b9f8538d4af6507d5cad26ac35caf6
Michael L. Young <elgueromexicano@gmail.com>
2024-05-16 08:20:09 -0400

    Fix change made to support dnf5
    Signed-off-by: Michael L. Young <elgueromexicano@gmail.com>

--- ci/fedora.sh
+++ ci/fedora.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-DNFCMD="dnf5"
-SPECFLAG=""
+DNFCMD="dnf"
+SPECFLAG="--spec "
 
-if [ "$OSVERSION" = "39" ]; then
-    DNFCMD="dnf"
-    SPECFLAG="--spec "
+if [ "$OSVERSION" = "rawhide" ] || [ "$((OSVERSION+0))" -gt 40 ]; then
+    DNFCMD="dnf5"
+    SPECFLAG=""
 fi
 
 $DNFCMD install -y rpm-build make gcc gcc-c++ libdrm-devel systemd-rpm-macros glibc-devel.x86_64 wget git $DNFCMD-command\(builddep\)
