999e0cab556b1a442cff78a3f07e2eb97fc15ec0
Michael L. Young <elgueromexicano@gmail.com>
2024-06-11 08:13:11 -0400

    Remove building for EL 8 distros
    Newer versions of DisplayLink Manager are built against a newer version of glibc
    than what is provided in these distros.
    
    CentOS Stream 8 is EOL as of May 31, 2024.
    
    Signed-off-by: Michael L. Young <elgueromexicano@gmail.com>

--- ci/almalinux.sh
+++ ci/almalinux.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-## Workaround AlmaLinux 8 GPG key issue
-## https://almalinux.org/blog/2023-12-20-almalinux-8-key-update/
-if [ "$OSVERSION" = "8" ]; then
-    rpm --import https://repo.almalinux.org/almalinux/RPM-GPG-KEY-AlmaLinux
-fi
-
 dnf install -y gcc gcc-c++ glibc-devel.x86_64 libdrm-devel rpm-build make wget dnf-utils git 'dnf-command(builddep)' --enablerepo=extras
 
 dnf builddep -y ./displaylink.spec
--- ci/centos-stream.sh
+++ ci/centos-stream.sh
@@ -2,11 +2,7 @@
 
 set -e
 
-if [ "$OSVERSION" = "stream9" ]; then
-    dnf install -y rpm-build make gcc glibc-devel.x86_64 wget git 'dnf-command(builddep)'
-else
-    dnf install -y gcc gcc-c++ glibc-devel.x86_64 libdrm-devel rpm-build make wget dnf-utils git --enablerepo=extras
-fi
+dnf install -y rpm-build make gcc glibc-devel.x86_64 wget git 'dnf-command(builddep)'
 
 dnf builddep -y ./displaylink.spec
 
