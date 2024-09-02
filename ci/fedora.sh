ac62ff37646bf3fa9a766a5037fddc978842ba6b
Mrinal Dhillon <mrinaldhillon@gmail.com>
2024-09-02 18:34:06 -0400

    add aarch64 support
    changes after review

--- ci/fedora.sh
+++ ci/fedora.sh
@@ -4,13 +4,17 @@ set -e
 
 DNFCMD="dnf"
 SPECFLAG="--spec "
-#ARCH="$(uname -m)"
 
 if [ "$OSVERSION" = "rawhide" ] || [ "$((OSVERSION + 0))" -gt 40 ]; then
 	DNFCMD="dnf5"
 	SPECFLAG=""
 fi
 
+# Note: Removed .x86_64 suffix from glibc-devel for aarch64 compatibility.
+# Previously, it was included to ensure that the x86_64 version of glibc-devel
+# was installed, particularly in cases where glibc-devel for i686 was present
+# and would not trigger the x86_64 install. This situation is rare but could
+# depend on specific user setups.
 $DNFCMD install -y rpm-build make gcc gcc-c++ libdrm-devel systemd-rpm-macros glibc-devel wget git "$DNFCMD-command(builddep)"
 
 $DNFCMD builddep -y $SPECFLAG./displaylink.spec
